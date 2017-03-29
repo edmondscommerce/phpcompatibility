43975430811a5432ca1e688cbd929482e7a5c6a1
Dan <Dan@Office>
2017-03-29 16:09:44 +0100

    Updated sniff runner

--- addSniffs.bash
+++ addSniffs.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 echo "Adding PHP Compatibility Sniff Path"
-php vendor/bin/phpcs --config-set installed_paths vendor/wimg/
+cp ./vendor/wimg/php-compatibility ./vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility -r
+#php vendor/bin/phpcs --config-set installed_paths vendor/wimg/
 echo "Done";
+
--- bin/php7.bash
+++ bin/php7.bash
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 cd $DIR;
 source "./_top.inc.bash"
@@ -13,7 +12,7 @@ Only PHP and PHTML files will be checked and all non-php files and JS etc will b
 
 Usage:
 
-./$0 [targetPath]
+./$0 [targetPath] (ignoreWarnings=false) (reportFile=\"report.json\")
 
 "
 }
@@ -24,9 +23,26 @@ then
     exit 1
 fi
 
-### Paramaters ###
+### Parameters ###
 targetPath="$1"
+ignoreWarnings=${2:-"false"}
+reportFile=${3:-"report.json"}
 
+commandFlags="--standard=PHPCompatibility \
+--runtime-set testVersion 7.0 \
+--extensions=php,phtml \
+--report-file=$reportFile \
+--report=json \
+-p";
+if [[ "$ignoreWarnings" == "true" ]]
+then
+    commandFlags="$commandFlags \
+    -n"
+fi
+
+arguments="$commandFlags $targetPath"
 echo "
 Running checks for PHP7 compatibility
 ";
+
+eval "$DIR/../vendor/bin/phpcs $arguments";
